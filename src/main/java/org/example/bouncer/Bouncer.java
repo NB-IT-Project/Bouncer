fa440679c299c8c21f236c239a2df94f6fecb4b8
NB-IT-Project <nberggold@gmail.com>
2024-11-13 19:15:50 +0100

    basic config

---
+++ src/main/java/org/example/bouncer/Bouncer.java
@@ -0,0 +1,9 @@
+package org.example.bouncer;
+
+import com.cthiebaud.passwordvalidator.*;
+
+public class Bouncer implements PasswordValidator {
+    public static void main(String[] args) {
+        System.out.println("dyfsasdfas");
+    }
+}
